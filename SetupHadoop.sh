974378d25ba7d87a804f00b96c17d575e354c23a
Jason Pollock <jason@pollock.ca>
2026-05-26 14:27:13 +1200

    Update the scripts to better reflect the tutorial.
    SetupHadoop.sh - base hadoop setup.
    SetupHadoopClasspath - classpath setup.
    
    Source one then the other.

---
+++ SetupHadoop.sh
@@ -0,0 +1,6 @@
+export HADOOP_VERSION=3.4.2
+export HADOOP_HOME=/local/Hadoop/hadoop-$HADOOP_VERSION
+export PATH=${PATH}:$HADOOP_HOME/bin
+export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
+
+need java8
